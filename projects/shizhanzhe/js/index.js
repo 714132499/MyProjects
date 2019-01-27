223f294b4e03324e2fe38d3378b4a6ae8fccd6ce
714132499 <15603833035@163.com>
2019-01-27 10:59:34 +0800

    'a'

---
+++ projects/shizhanzhe/js/index.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper('.swiper-container', {
+    centeredSlides: true,
+    autoplay: {
+        delay: 2000,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
---
+++ readme.md
@@ -0,0 +1,11 @@
+**bootstrap 4**
+
+**js插件**
+1、Pace.js 页面加载进度条
+2、FullPage.js 全屏插件
+3、Swiper.js 轮播插件
+
+**css 预处理**
+1、Less 
+
+
